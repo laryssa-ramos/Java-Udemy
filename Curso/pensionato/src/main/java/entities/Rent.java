0e91bf61a92051a1352e990643c68f5df646d114
Laryssa Ramos <laryssaramos284@gmail.com>
2024-04-26 06:46:22 -0300

    For Each

---
+++ Curso/pensionato/src/main/java/entities/Rent.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author Laryssa
+ */
+public class Rent {
+    
+    private String name;
+    private String email;
+    
+    public Rent(){
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Rent(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return name + "," + email ;
+    }
+    
+    
+}
