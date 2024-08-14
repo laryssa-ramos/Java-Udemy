ebb5a6ff8630c19e6b633ea38f7e5627f670520f
Laryssa Ramos <laryssaramos284@gmail.com>
2024-08-14 08:06:20 -0300

    188.ChessException e ChessPosition - ChessException and ChessPosition

--- chess-system/src/main/java/chess/ChessMatch.java
+++ chess-system/src/main/java/chess/ChessMatch.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package chess;
 
 import boardgame.Board;
@@ -9,10 +5,6 @@ import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
-/**
- *
- * @author Laryssa
- */
 public class ChessMatch {
     
     private Board board;
@@ -33,10 +25,14 @@ public class ChessMatch {
             return mat;
     }
     
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+    
     private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0,4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7,4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8 ,new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     } // método para iniciar a partida de xadrez
     
 }
