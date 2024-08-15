d61302858550d8bb8153073d356b7880bde898c3
Laryssa Ramos <laryssaramos284@gmail.com>
2024-08-15 07:18:46 -0300

    190.Movendo peças - Moving pieces

--- chess-system/src/main/java/boardgame/Board.java
+++ chess-system/src/main/java/boardgame/Board.java
@@ -62,6 +62,21 @@ public class Board {
         piece.position = position;
     }
     
+    
+    //Função para remover a peça da posição(mover)
+    public  Piece removePiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position not on the board");
+        }
+        if(piece(position) == null){
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
+    }
+    
     private boolean positionExists(int row, int column){
         return row >= 0 && row < rows && column >= 0 && column < columns;
     }
