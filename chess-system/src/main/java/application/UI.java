d687247a86be64e5dc6ae474f71e50d180c06853
Laryssa Ramos <laryssaramos284@gmail.com>
2024-08-16 06:27:46 -0300

    191.Tratando excecoes e limpando a tela - Handling exceptions and clearing screen

--- chess-system/src/main/java/application/UI.java
+++ chess-system/src/main/java/application/UI.java
@@ -38,6 +38,15 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
     
     //Função para ler a posição vinda do usuário
+    
+    // https://stackoverflow.com/questions/2979383/java-clear-the-console
+    
+    // Função para limpar o terminal
+    public static void clearScreen(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+        
     public static ChessPosition readChessPosition(Scanner sc){
         try{
             String s = sc.nextLine();
