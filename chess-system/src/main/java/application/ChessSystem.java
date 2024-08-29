980841627eb04024e3ab922dc77689a4861462a0
Laryssa Ramos <laryssaramos284@gmail.com>
2024-08-29 06:09:52 -0300

    200.Lógica de xequemate - Checkmate Logic - 201.Contagem de movimentos - Piece move count

--- chess-system/src/main/java/application/ChessSystem.java
+++ chess-system/src/main/java/application/ChessSystem.java
@@ -25,7 +25,7 @@ public class ChessSystem {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
         
-        while(true){
+        while(!chessMatch.getCheckMate()){
          try{
              UI.clearScreen();
              UI.printMatch(chessMatch, captured);
@@ -58,5 +58,8 @@ public class ChessSystem {
          }
             
         }
+        
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
